deebfd3504f67a0024394a917d33effe1154e291
Utkarsh Singh <utkarshsingh@Utkarshs-MacBook-Air.local>
2022-08-13 17:07:38 +0530

    eslint fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
