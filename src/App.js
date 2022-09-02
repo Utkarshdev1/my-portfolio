bb1cafc92a92758c9ec0961f48f40764fad25c1d
Utkarsh Singh <utkarshsingh@Utkarshs-MacBook-Air.local>
2022-09-02 23:52:52 +0530

    projects and nabbar added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import About from "./components/About";
-// import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 // import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      {/* <Navbar /> */}
+      <Navbar />
       <About />
-      {/* <Projects /> */}
+      <Projects />
       <Skills />
       {/* <Testimonials /> */}
-      {/* <Contact /> */}
+      <Contact />
     </main>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,3 +1,6 @@
+import Port from "./Images/port1.png";
+import Port2 from "./Images/port2.png";
+
 export const skills = [
   "React Native",
   "React",
@@ -9,4 +12,23 @@ export const skills = [
   "xCode",
   "Android Studio",
   "Firebase",
+];
+
+export const projects = [
+  {
+    link: "https://www.getwork.org/one-app",
+    image: Port,
+    subtitle: "Job & Internship Search",
+    title: "GetWork OneApp",
+    description:
+      "A job search platform GetWork does indeed meet all of your needs. As a one-stop shop for all your job search needs, GetWork app is the best option for those seeking part-time, remote, or working from home positions, local jobs , freelancing gigs as well as learning opportunities.",
+  },
+  {
+    link: "https://play.google.com/store/apps/details?id=com.drorapp",
+    image: Port2,
+    subtitle: "Report & Win Rewards",
+    title: "Lythouse",
+    description:
+      "Lythouse is your personal safety app to help make your city safer. Stay updated with the local incidents related to public and personal safety around you with us. ",
+  },
 ];
